f89e801cc44d2e918ea81edce5137d820c832905
mipromptingeniering-alt <mipromptingeniering@gmail.com>
2026-02-13 09:28:45 +0100

    Update __init__.py

---
+++ agents/__init__.py
@@ -0,0 +1,13 @@
+from . import researcher_agent
+from . import generator_agent
+from . import critic_agent
+from . import optimizer_agent
+from . import report_agent
+
+__all__ = [
+    'researcher_agent',
+    'generator_agent',
+    'critic_agent',
+    'optimizer_agent',
+    'report_agent'
+]
