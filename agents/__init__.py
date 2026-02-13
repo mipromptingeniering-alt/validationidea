46f341c993963c852d346f5e7550afc99aa0942a
mipromptingeniering-alt <mipromptingeniering@gmail.com>
2026-02-13 17:35:41 +0100

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -6,14 +6,3 @@ from . import report_agent
 from . import landing_generator
 from . import dashboard_generator
 from . import telegram_notifier
-
-__all__ = [
-    'researcher_agent',
-    'generator_agent',
-    'critic_agent',
-    'optimizer_agent',
-    'report_agent',
-    'landing_generator',
-    'dashboard_generator',
-    'telegram_notifier'
-]
