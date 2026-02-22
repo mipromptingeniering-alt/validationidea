769012298110b75e308aa8e9c23609e4154c7800
mipromptingeniering-alt <mipromptingeniering@gmail.com>
2026-02-22 20:05:16 +0100

    fix: pytz en requirements + limpiar agents/__init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,8 +1,1 @@
-from . import researcher_agent
-from . import generator_agent
-from . import critic_agent
-from . import optimizer_agent
-from . import report_agent
-from . import landing_generator
-from . import dashboard_generator
-from . import telegram_notifier
+
